f5eac9c338f10c27b9285bd4d53a288787bb6a79
gmuffiness <gmuffiness@gmail.com>
2022-02-23 19:06:45 +0900

    Update data/gaussian module for readability
    - Use more intuitive variable, function naming
    - Reformat with black
    - Align generate_region, generate_affinity function

--- data/gaussian.py
+++ data/gaussian.py
@@ -1,252 +1,155 @@
-# coding=utf-8
-from math import exp
 import numpy as np
 import cv2
-import os
-import math
-import sys
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
 from data.boxEnlarge import enlargebox
 
 
-class GaussianTransformer(object):
+class GaussianBuilder(object):
+    def __init__(self, init_size, sigma):
+        self.init_size = init_size
+        self.sigma = sigma
+        self.gaussian_map, self.gaussian_map_color = self.generate_gaussian_map()
 
-    def __init__(self, imgSize=200, enlargeSize=1.50, sigma=40):
-        self.imgSize = imgSize
-        isotropicGrayscaleImage, isotropicGrayscaleImageColor = self.gen_gaussian_heatmap(sigma)
-        self.standardGaussianHeat = isotropicGrayscaleImage
-        self.enlargeSize = enlargeSize
-        # color_gaussian = cv2.applyColorMap(self.standardGaussianHeat, cv2.COLORMAP_JET)
-        # cv2.imshow("test", color_gaussian)
-        # cv2.waitKey(0)
-        # self._test()
+    def generate_gaussian_map(self):
+        circle_mask = self.generate_circle_mask()
 
-    def gen_gaussian_heatmap(self, sigma):
-        circle_mask = self.gen_circle_mask()
-        imgSize = self.imgSize
-        isotropicGrayscaleImage = np.zeros((imgSize, imgSize), np.float32)
+        gaussian_map = np.zeros((self.init_size, self.init_size), np.float32)
 
+        for i in range(self.init_size):
+            for j in range(self.init_size):
+                gaussian_map[i, j] = 1 / 2 / np.pi / (self.sigma ** 2) * np.exp(
+                    -1 / 2 * ((i - self.init_size / 2) ** 2 / (self.sigma ** 2) + (j - self.init_size / 2) ** 2 / (self.sigma ** 2)))
 
-        sigma = sigma
-        for i in range(imgSize):
-            for j in range(imgSize):
-                isotropicGrayscaleImage[i, j] = 1 / 2 / np.pi / (sigma ** 2) * np.exp(
-                    -1 / 2 * ((i - imgSize / 2) ** 2 / (sigma ** 2) + (j - imgSize / 2) ** 2 / (sigma ** 2)))
-        # 如果要可视化对比正方形和最大内切圆高斯图的区别，注释下面这行即可
-        isotropicGrayscaleImage = isotropicGrayscaleImage * circle_mask
-        isotropicGrayscaleImage = (isotropicGrayscaleImage / np.max(isotropicGrayscaleImage)).astype(np.float32)
+        gaussian_map = gaussian_map * circle_mask
+        gaussian_map = (gaussian_map / np.max(gaussian_map)).astype(np.float32)
 
-        isotropicGrayscaleImage = (isotropicGrayscaleImage / np.max(isotropicGrayscaleImage) * 255).astype(np.uint8)
-        isotropicGrayscaleImageColor = cv2.applyColorMap(isotropicGrayscaleImage, cv2.COLORMAP_JET)
+        gaussian_map_color = (gaussian_map * 255).astype(np.uint8)
+        gaussian_map_color = cv2.applyColorMap(gaussian_map_color, cv2.COLORMAP_JET)
 
-        return isotropicGrayscaleImage, isotropicGrayscaleImageColor
+        return gaussian_map, gaussian_map_color
 
+    def generate_circle_mask(self):
 
-    # 生成高斯图的mask，对于正方形的高斯图来说，只将最大内切圆作为字符的高斯图区域去学习
-    # 在初版开源的高斯图生成中，是将正方形完整区域作为高斯图的
-    # 新的方法可视化后与作者论文中展示的可视化效果是完全一致的
-    def gen_circle_mask(self):
-        imgSize = self.imgSize
-        circle_img = np.zeros((imgSize, imgSize), np.float32)
-        circle_mask = cv2.circle(circle_img, (imgSize//2, imgSize//2), imgSize//2, 1, -1)
+        zero_arr = np.zeros((self.init_size, self.init_size), np.float32)
+        circle_mask = cv2.circle(
+            img=zero_arr,
+            center=(self.init_size // 2, self.init_size // 2),
+            radius=self.init_size // 2,
+            color=1,
+            thickness=-1,
+        )
 
         return circle_mask
 
-    # 将原始的box扩大1.5倍
-    # 仅仅作用于正矩形，不规则四边形无效， 请参考data/boEnlarge文件
-    def enlargeBox(self, box, imgh, imgw):
-        boxw = box[1][0] - box[0][0]
-        boxh = box[2][1] - box[1][1]
-
-        if imgh <= boxh or imgw <= boxw:
-            return box
-
-        enlargew = boxw * 0.5
-        enlargeh = boxh * 0.5
-
-        # box扩大这部分为了清晰，code写的比较冗余
-        # 左上角顶点扩充后坐标， 剩下点顺时针以此类推
-        box[0][0], box[0][1] = max(0, box[0][0] - int(enlargew*0.5)), max(0, box[0][1] - int(enlargeh*0.5))
-        box[1][0], box[1][1] = min(imgw, box[1][0] + int(enlargew*0.5)), max(0, box[1][1] - int(enlargeh*0.5))
-        box[2][0], box[2][1] = min(imgw, box[2][0] + int(enlargew*0.5)), min(imgh, box[2][1] + int(enlargeh*0.5))
-        box[3][0], box[3][1] = max(0, box[3][0] - int(enlargew*0.5)), min(imgh, box[3][1] + int(enlargeh*0.5))
-
-        return box
-
-    def four_point_transform(self, target_bbox, save_dir=None):
+    def four_point_transform(self, bbox):
         """
-        Using the pts and the image a perspective transform is performed which returns the transformed 2d Gaussian image
-        :param target_bbox: np.array, dtype=np.uint8, shape = [height, width]
-        :param save_dir: np.array, dtype=np.float32 or np.int32, shape = [4, 2]
-        :return:
+        Using the pts and the self.gaussian_map, returns Transformed 2d Gaussian map
         """
+        width, height = (
+            np.max(bbox[:, 0]).astype(np.int32),
+            np.max(bbox[:, 1]).astype(np.int32),
+        )
+        init_points = np.array(
+            [
+                [0, 0],
+                [self.init_size, 0],
+                [self.init_size, self.init_size],
+                [0, self.init_size],
+            ],
+            dtype="float32",
+        )
 
-        width, height = np.max(target_bbox[:, 0]).astype(np.int32), np.max(target_bbox[:, 1]).astype(np.int32)
+        M = cv2.getPerspectiveTransform(init_points, bbox)
+        warped_gaussian_map = cv2.warpPerspective(self.gaussian_map, M, (width, height))
+        warped_gaussian_map = np.array(warped_gaussian_map * 255, np.uint8)
+        return warped_gaussian_map, width, height
 
-        # if width == 0:
-        #     width = 1
-        # if height == 0:
-        #     height = 1
-
-        right = self.standardGaussianHeat.shape[1] - 1
-        bottom = self.standardGaussianHeat.shape[0] - 1
-        ori = np.array([[0, 0], [right, 0],
-                        [right, bottom],
-                        [0, bottom]], dtype="float32")
-        M = cv2.getPerspectiveTransform(ori, target_bbox)
-        warped = cv2.warpPerspective(self.standardGaussianHeat.copy(), M, (int(width), int(height)))
-        warped = np.array(warped, np.uint8)
-        if save_dir:
-            warped_color = cv2.applyColorMap(warped, cv2.COLORMAP_JET)
-            cv2.imwrite(os.path.join(save_dir, 'warped.jpg'), warped_color)
-
-        return warped, width, height
-
-    def add_character(self, image, bbox, signal = None):
+    def add_gaussian_map_to_image(self, image, bbox, map_type=None):
 
         """
         mapping Gaussian heat maps to the character box coordinates of the image.
-        :param target_bbox: np.array, dtype=np.uint8, shape = [height, width]
-        :param save_dir: np.array, dtype=np.float32 or np.int32, shape = [4, 2]
-        :return:
         """
 
+        if map_type == "region":
+            # TODO : edit enlargebox output type from int 32 to float32
+            bbox = enlargebox(bbox, image.shape[0], image.shape[1])
 
-        #bbox = self.enlargeBox(bbox, image.shape[0], image.shape[1])
-        bbox_copy = bbox.copy()
-        # import ipdb;ipdb.set_trace()
-        bbox = enlargebox(bbox, image.shape[0], image.shape[1])
-
-        if signal == "affinity":
-            bbox[0][0], bbox[1][0], bbox[2][0], bbox[3][0] = \
-                bbox_copy[0][0], bbox_copy[1][0], bbox_copy[2][0], bbox_copy[3][0]
-
-        if np.any(bbox < 0) or np.any(bbox[:, 0] > image.shape[1]) or np.any(bbox[:, 1] > image.shape[0]):
+        if (
+            np.any(bbox < 0)
+            or np.any(bbox[:, 0] > image.shape[1])
+            or np.any(bbox[:, 1] > image.shape[0])
+        ):
             return image
 
-        top_left = np.array([np.min(bbox[:, 0]), np.min(bbox[:, 1])]).astype(np.int32)
-        bbox -= top_left[None, :]
-        transformed, width, height = self.four_point_transform(bbox.astype(np.float32))
-
-
-        # if width * height < 10:
-        #     return image
+        bbox_left, bbox_top = np.array([np.min(bbox[:, 0]), np.min(bbox[:, 1])]).astype(np.int32)
+        bbox -= (bbox_left, bbox_top)
+        warped_gaussian_map, width, height = self.four_point_transform(
+            bbox.astype(np.float32)
+        )
 
         try:
-            score_map = image[top_left[1]:top_left[1] + transformed.shape[0],
-                        top_left[0]:top_left[0] + transformed.shape[1]]
-            score_map = np.where(transformed > score_map, transformed, score_map)
-            image[top_left[1]:top_left[1] + transformed.shape[0],
-            top_left[0]:top_left[0] + transformed.shape[1]] = score_map
-
-
+            bbox_area_of_image = image[
+                bbox_top : bbox_top + height, bbox_left : bbox_left + width,
+            ]
+            score_map = np.where(
+                warped_gaussian_map > bbox_area_of_image,
+                warped_gaussian_map,
+                bbox_area_of_image,
+            )
+            image[
+                bbox_top : bbox_top + height, bbox_left : bbox_left + width,
+            ] = score_map
 
         except Exception as e:
-            # print('tansformed shape:{}\n image top_left shape:{}\n top transformed shape:{}\n width and hright:{}
-            # \n ori box:{}\n top left:{}\n point:{}\n min width height:{}\n bbox:{}\n'
-            #       .format(transformed.shape, image[top_left[1]:top_left[1],
-            # top_left[0]:top_left[0]].shape, image[top_left[1]:top_left[1] + transformed.shape[0],
-            # top_left[0]:top_left[0] + transformed.shape[1]].shape, (width, height), ori_box, top_left,point,
-            #       np.array([np.min(ori_box[:, 0]), np.min(ori_box[:, 1])]).astype(np.int32),
-            #       ori_box-np.array([np.min(ori_box[:, 0]), np.min(ori_box[:, 1])]).astype(np.int32)))
-
-            # import ipdb;ipdb.set_trace()
-            print('second filter {} {} {}'.format(width,height,signal))
-            print('error message {}'.format(e))
+            print("Error : {}".format(e))
+            print(
+                "On generating {} map, strange box came out. (width: {}, height: {})".format(
+                    map_type, width, height
+                )
+            )
         return image
 
-    def add_affinity(self, image, bbox_1, bbox_2):
+    def calculate_affinity_box_points(self, bbox_1, bbox_2):
         center_1, center_2 = np.mean(bbox_1, axis=0), np.mean(bbox_2, axis=0)
         tl = (bbox_1[0:2].sum(0) + center_1) / 3
         bl = (bbox_2[0:2].sum(0) + center_2) / 3
         tr = (bbox_2[2:4].sum(0) + center_2) / 3
         br = (bbox_1[2:4].sum(0) + center_1) / 3
 
-        affinity = np.array([tl, bl, tr, br]).astype(np.float32)
+        # for visualize
         # tl = np.mean([bbox_1[0], bbox_1[1], center_1], axis=0)
         # bl = np.mean([bbox_1[2], bbox_1[3], center_1], axis=0)
         # tr = np.mean([bbox_2[0], bbox_2[1], center_2], axis=0)
         # br = np.mean([bbox_2[2], bbox_2[3], center_2], axis=0)
-        #
-        # affinity = np.array([tl, tr, br, bl])
+        bbox = np.array([tl, bl, tr, br]).astype(np.float32)
 
-        return self.add_character(image, affinity.copy(), signal='affinity'), np.expand_dims(affinity, axis=0)
+        return bbox
 
-    def generate_region(self, image_size, bboxes, signal='none'):
-        height, width, channel = image_size
-        target = np.zeros([height, width], dtype=np.float32)
-        for i in range(len(bboxes)):
-            character_bbox = np.array(bboxes[i])
-            for j in range(bboxes[i].shape[0]):
-                #target = self.add_character(target, character_bbox[j], singal='region')
-                target = self.add_character(target, character_bbox[j], signal=signal)
+    def generate_region(self, img_h, img_w, word_level_char_bbox):
 
-        return target
+        region_map = np.zeros([img_h, img_w], dtype=np.float32)
+        for i in range(len(word_level_char_bbox)):
+            for j in range(len(word_level_char_bbox[i])):
+                region_map = self.add_gaussian_map_to_image(
+                    region_map, word_level_char_bbox[i][j].copy(), map_type="region"
+                )
+        return region_map
 
-    def saveGaussianHeat(self):
-        images_folder = os.path.abspath(os.path.dirname(__file__)) + '/images'
-        cv2.imwrite(os.path.join(images_folder, 'standard.jpg'), self.standardGaussianHeat)
-        warped_color = cv2.applyColorMap(self.standardGaussianHeat, cv2.COLORMAP_JET)
-        cv2.imwrite(os.path.join(images_folder, 'standard_color.jpg'), warped_color)
-        standardGaussianHeat1 = self.standardGaussianHeat.copy()
-        standardGaussianHeat1[standardGaussianHeat1 < (0.4 * 255)] = 255
-        threshhold_guassian = cv2.applyColorMap(standardGaussianHeat1, cv2.COLORMAP_JET)
-        cv2.imwrite(os.path.join(images_folder, 'threshhold_guassian.jpg'), threshhold_guassian)
+    def generate_affinity(self, img_h, img_w, word_level_char_bbox):
 
-    def generate_affinity(self, image_size, bboxes, words):
-        height, width, channel = image_size
-        target = np.zeros([height, width], dtype=np.float32)
-        affinities = []
-        for i in range(len(words)):
-            character_bbox = np.array(bboxes[i])
-            total_letters = 0
-            for char_num in range(character_bbox.shape[0] - 1):
-                target, affinity = self.add_affinity(target, character_bbox[total_letters],
-                                                     character_bbox[total_letters + 1])
-                affinities.append(affinity)
-                total_letters += 1
-        if len(affinities) > 0:
-            affinities = np.concatenate(affinities, axis=0)
-        return target, affinities
+        affinity_map = np.zeros([img_h, img_w], dtype=np.float32)
+        all_affinity_bbox = []
+        for i in range(len(word_level_char_bbox)):
+            for j in range(len(word_level_char_bbox[i]) - 1):
+                affinity_bbox = self.calculate_affinity_box_points(
+                    word_level_char_bbox[i][j], word_level_char_bbox[i][j + 1],
+                )
 
+                affinity_map = self.add_gaussian_map_to_image(
+                    affinity_map, affinity_bbox.copy(), map_type="affinity"
+                )
+                all_affinity_bbox.append(np.expand_dims(affinity_bbox, axis=0))
 
-if __name__ == '__main__':
-    # gaussian = GaussianTransformer(200, 1.5)
-    # gaussian.saveGaussianHeat()
-    image = np.zeros((500,500,3),dtype=np.uint8)
-
-    gen = GaussianTransformer(imgSize=200, enlargeSize=1.50)
-    gen.gen_circle_mask()
-    bbox = np.array([[[60, 140], [90, 160], [90, 260], [60, 230]], [[110, 165], [180, 165], [180, 255], [110, 255]], [[167, 164],
-       [259, 154],
-       [262, 178],
-       [168, 188]]])
-
-    bbox = bbox[np.newaxis, :, :,:]
-    region_image = gen.generate_region(image.shape, bbox).astype(np.uint8)
-    # import ipdb;ipdb.set_trace()
-    region_image = cv2.applyColorMap(region_image, cv2.COLORMAP_JET)
-    affinity_image, affinities = gen.generate_affinity(image.shape, bbox, [[1,2]])
-    affinity_image = affinity_image.astype(np.uint8)
-    affinity_image = cv2.applyColorMap(affinity_image, cv2.COLORMAP_JET)
-    target_bbox = np.array([[45, 135], [135, 135], [135, 295], [45, 295]], dtype=np.int8)
-
-    for boxes in bbox:
-        for box in boxes:
-            # cv2.rectangle(image, tuple(box[0]), tuple(box[2]), (0, 255, 255), 2)
-            enlarge = enlargebox(box, image.shape[1], image.shape[0])
-            cv2.polylines(region_image, [box], True, (0, 255, 255), 2)
-            cv2.polylines(region_image, [enlarge], True, (0, 0, 255), 2)
-            cv2.polylines(affinity_image, [box], True, (0, 255, 255), 2)
-            # cv2.polylines(affinity_image, [enlarge], True, (0, 0, 255), 2)
-
-    cv2.polylines(affinity_image, [affinities[0].astype(np.int)], True, (255, 0, 255), 2)
-    cv2.imwrite('/nas/home/gmuffiness/result/test_gaussian_y_1.25.jpg', np.hstack((region_image, affinity_image)))
-    # cv2.imshow('test', np.hstack((region_image, affinity_image)))
-    # cv2.waitKey(0)
-
-    # weight, target = gaussian.generate_target((1024, 1024, 3), bbox.copy())
-    # target_gaussian_heatmap_color = imgproc.cvt2HeatmapImg(weight.copy() / 255)
-    # cv2.imshow('test', target_gaussian_heatmap_color)
-    # cv2.waitKey()
-    # cv2.imwrite("test.jpg", target_gaussian_heatmap_color)
+        if len(all_affinity_bbox) > 0:
+            all_affinity_bbox = np.concatenate(all_affinity_bbox, axis=0)
+        return affinity_map, all_affinity_bbox
