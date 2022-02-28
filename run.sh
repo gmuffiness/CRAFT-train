680bc2165e4d692327b9ee52ca2b20e0cd4b49c8
DESKTOP-C123MLD\woans <woans0104@gmail.com>
2022-02-28 10:26:12 +0900

    commit test

--- run.sh
+++ run.sh
@@ -1,5 +1,17 @@
-cd /nas/home/gmuffiness/workspace/ocr_related/daintlab-CRAFT-Reimplementation/clean-code/unittest
-python3 dataloader_test.py
-echo $PYTHONPATH
-export PYTHONPATH=$PYTHONPATH:$/nas/home/gmuffiness/workspace/ocr_related/daintlab-CRAFT-Reimplementation/clean-code/config
-vim ~/.bashrc
+#!/bin/bash
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export PYTHONIOENCODING=UTF-8
+
+
+
+SET=$(seq 1 1 1)
+for i in $SET
+
+do
+
+  CUDA_VISIBLE_DEVICES=0,1,2,3 python3 trainSynth_dp.py --yaml="synth_shwang" \
+
+
+
+done
