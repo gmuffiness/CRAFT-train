57fab060e761fe3c5c5b4c067f4d54c4c15f3b61
gmuffiness <gmuffiness@gmail.com>
2022-03-04 00:03:07 +0900

    Remove correcting process of GT bbox to parallelogram

--- utils/inference_boxes.py
+++ utils/inference_boxes.py
@@ -11,7 +11,6 @@ from torch.autograd import Variable
 from utils.craft_utils import getDetBoxes, adjustResultCoordinates
 from data import imgproc
 from data.dataset import SynthTextDataSet
-from data.pointClockOrder import mep
 
 def load_synthtext_gt(data_folder):
 
@@ -42,11 +41,11 @@ def load_synthtext_gt(data_folder):
 
         single_img_bboxes = []
         for j in range(len(words)):
-            boxInfos = {"points": None, "text": None, "ignore": None}
-            boxInfos["points"] = wordbox[j]
-            boxInfos["text"] = words[j]
-            boxInfos["ignore"] = False
-            single_img_bboxes.append(boxInfos)
+            box_info_dict = {"points": None, "text": None, "ignore": None}
+            box_info_dict["points"] = wordbox[j]
+            box_info_dict["text"] = words[j]
+            box_info_dict["ignore"] = False
+            single_img_bboxes.append(box_info_dict)
 
         total_imgs_bboxes.append(single_img_bboxes)
 
@@ -75,36 +74,24 @@ def load_icdar2015_gt(dataFolder, isTraing=False):
         lines = open(gt_path, encoding="utf-8").readlines()
         single_img_bboxes = []
         for line in lines:
-            boxInfos = {"points": None, "text": None, "ignore": None}
+            box_info_dict = {"points": None, "text": None, "ignore": None}
 
-            ori_box = line.strip().encode("utf-8").decode("utf-8-sig").split(",")
-            box = [int(ori_box[j]) for j in range(8)]
-            word = ori_box[8:]
+            box_info = line.strip().encode("utf-8").decode("utf-8-sig").split(",")
+            box_points = [int(box_info[j]) for j in range(8)]
+            word = box_info[8:]
             word = ",".join(word)
-            box = np.array(box, np.int32).reshape(4, 2)
-            area, p0, p3, p2, p1, _, _ = mep(box)
-            bbox = np.array([p0, p1, p2, p3])
-            distance = 10000000
-            index = 0
-            for i in range(4):
-                d = np.linalg.norm(box[0] - bbox[i])
-                if distance > d:
-                    index = i
-                    distance = d
-            new_box = []
-            for i in range(index, index + 4):
-                new_box.append(bbox[i % 4])
+            box_points = np.array(box_points, np.int32).reshape(4, 2)
             cv2.polylines(
-                image, [np.array(new_box).astype(np.int)], True, (0, 0, 255), 1
+                image, [np.array(box_points).astype(np.int)], True, (0, 0, 255), 1
             )
-            boxInfos["points"] = new_box
-            boxInfos["text"] = word
+            box_info_dict["points"] = box_points
+            box_info_dict["text"] = word
             if word == "###":
-                boxInfos["ignore"] = True
+                box_info_dict["ignore"] = True
             else:
-                boxInfos["ignore"] = False
+                box_info_dict["ignore"] = False
 
-            single_img_bboxes.append(boxInfos)
+            single_img_bboxes.append(box_info_dict)
         total_imgs_bboxes.append(single_img_bboxes)
         total_img_path.append(img_path)
     return total_imgs_bboxes, total_img_path
@@ -135,11 +122,11 @@ def load_icdar2013_gt(dataFolder, isTraing=False):
         lines = open(gt_path, encoding="utf-8").readlines()
         single_img_bboxes = []
         for line in lines:
-            boxInfos = {"points": None, "text": None, "ignore": None}
+            box_info_dict = {"points": None, "text": None, "ignore": None}
 
-            ori_box = line.strip().encode("utf-8").decode("utf-8-sig").split(",")
-            box = [int(ori_box[j]) for j in range(4)]
-            word = ori_box[4:]
+            box_info = line.strip().encode("utf-8").decode("utf-8-sig").split(",")
+            box = [int(box_info[j]) for j in range(4)]
+            word = box_info[4:]
             word = ",".join(word)
             box = [
                 [box[0], box[1]],
@@ -148,14 +135,14 @@ def load_icdar2013_gt(dataFolder, isTraing=False):
                 [box[0], box[3]],
             ]
 
-            boxInfos["points"] = box
-            boxInfos["text"] = word
+            box_info_dict["points"] = box
+            box_info_dict["text"] = word
             if word == "###":
-                boxInfos["ignore"] = True
+                box_info_dict["ignore"] = True
             else:
-                boxInfos["ignore"] = False
+                box_info_dict["ignore"] = False
 
-            single_img_bboxes.append(boxInfos)
+            single_img_bboxes.append(box_info_dict)
 
         total_imgs_bboxes.append(single_img_bboxes)
         total_img_path.append(img_path)
